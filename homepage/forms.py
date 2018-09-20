6f0b03cee4b4acf6776a583758856e0bdb1b2bad
prince <prince@nuventure.in>
2018-09-20 15:33:09 +0530

    html rendering

--- homepage/forms.py
+++ homepage/forms.py
@@ -1,11 +1,12 @@
 from django import forms
 from .models import Post
+from sanitizer.forms import SanitizedCharField
 
 
 class Blog(forms.ModelForm):
     post_tittle = forms.CharField(max_length=100,
                                   widget=forms.TextInput(attrs={'class': 'form-control' , 'pattern':'.{10,}'}))
-    post_content = forms.CharField(label='content',
+    post_content = SanitizedCharField(label='content',allowed_tags=['b', 'h1','h2','h3','h4','h5','h6', 'i'],
                                    max_length=2000,
                                    widget=forms.Textarea(attrs={'class': 'form-control','minlength':'10'}),
                                    help_text='Write here your message!'
