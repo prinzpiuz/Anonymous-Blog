a39e25837ae88125b85a147ed21578cc12c7373c
prince <prince@nuventure.in>
2018-09-25 16:25:32 +0530

    fixed issue in test case

--- homepage/tests.py
+++ homepage/tests.py
@@ -81,14 +81,14 @@ class Rendering(TestCase):
 
     def setUp(self):
         self.input = {'post_tittle': 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
-                      'post_content': 'this tag will not render<a>\n\nthis is for test'}
+                      'post_content': 'this tag will not render<a></a>\n\nthis is for test'}
 
         self.input1 = {'post_tittle': 'aaaaaaaaaaa', 'post_content': '<h1>will</h1><h2>hi</h2>'}
 
 
     def test_multiple_para_rendering_and_NO_unsafe_tags(self):
         response = self.client.post(reverse('homepage:create'), self.input, follow=True)
-        self.assertContains(response, '<p>this tag will not render&lt;a&gt;</p>', status_code=200)
+        self.assertContains(response, '<p>this tag will not render&lt;a&gt;&lt;/a&gt;</p>\n\n<p>this is for test</p>', status_code=200)
 
     def test_TOC(self):
         response = self.client.post(reverse('homepage:create'), self.input1, follow=True)
