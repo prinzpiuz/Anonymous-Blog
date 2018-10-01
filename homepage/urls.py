89e5efcd81b9ac04c1c541bfcb2deb4d0b748bfd
prince <prince@nuventure.in>
2018-10-01 12:54:26 +0530

    added edit link for logged in user

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from homepage.views import Create, post, Edit, Posts, Register, LoginUser, LogOut, Mine
-
+from homepage.views import Create, post, Edit, Posts, Register, LoginUser, LogOut, Mine, LogedInUserEdit
 
 app_name = 'homepage'
 
@@ -9,8 +8,10 @@ urlpatterns = [
     path('login/', LoginUser.as_view(), name='login'),
     path('', Create.as_view(), name='create'),
     path('post/<id>/', post, name='post'),
+    path('<id>/edit/', LogedInUserEdit.as_view(), name='loginedit'),
     path('<id>/edit/<skey>', Edit.as_view(), name='edit'),
     path('posts/', Posts.as_view(), name='posts'),
-    path('logout/',LogOut.as_view(), name='logout'),
-    path('mine/',Mine.as_view(), name='mine')
+    path('logout/', LogOut.as_view(), name='logout'),
+    path('mine/', Mine.as_view(), name='mine')
+
 ]
